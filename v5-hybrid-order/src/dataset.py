9b89f07e00224b4fd833455961b56b385d3d6d79
calexin <alexin.choisnet@gmail.com>
2025-11-08 22:22:53 +0100

    Only use subset of the dataset

--- v5-hybrid-order/src/dataset.py
+++ v5-hybrid-order/src/dataset.py
@@ -6,13 +6,11 @@ from torch.utils.data import Dataset
 from tqdm import tqdm
 
 class OpticalSimulationDatasetPD(Dataset):
-    def __init__(self, data_file=None, transform=None, normalize=False, logger=None, device='cpu', dtype=torch.complex64):
+    def __init__(self, data_file=None, transform=None, num_samples=0, logger=None, device='cpu', dtype=torch.complex64):
         """
         Args:
             data_file: path to saved dataset file (.pkl).
             transform: optional callable to apply to each sample.
-            fft: compute FFTs for perm_map and field arrays when loading.
-            normalize: whether to compute & store normalization stats and normalized arrays.
             logger: optional logger instance.
             device: device for tensors ('cpu' or 'cuda').
             dtype: torch dtype for numeric tensors.
@@ -24,7 +22,7 @@ class OpticalSimulationDatasetPD(Dataset):
         self.dtype = dtype
 
         if data_file:
-            self.load_dataset(data_file, normalize=normalize)
+            self.load_dataset(data_file, num_samples=num_samples)
 
     def __len__(self):
         return 0 if self.df is None else len(self.df)
@@ -57,11 +55,16 @@ class OpticalSimulationDatasetPD(Dataset):
             print(f"Dataset saved to {output_file}")
         return self
 
-    def load_dataset(self, input_file, normalize=False):
+    def load_dataset(self, input_file, num_samples=0):
         if not input_file.endswith('.csv'):
             raise ValueError("Input file must end with .csv")
 
         self.df = pd.read_csv(input_file).reset_index()
+
+        if num_samples > 0 and num_samples < len(self.df):
+            N = np.array(self.df.index)
+            random_indices = np.random.choice(N, size=int(num_samples), replace=False)
+            self.df = self.df.iloc[random_indices].reset_index(drop=True)
 
         # Transmissions and reflections are stored as dict 'real', 'imag'. Convert to complex.
         for col in tqdm(self.df.columns, desc="Loading dataset features"):
@@ -69,7 +72,10 @@ class OpticalSimulationDatasetPD(Dataset):
                 self.df[col] = self.df[col].apply(lambda x: complex(eval(x)['real'], eval(x)['imag']))
 
         if self.logger:
-            self.logger.info(f"Loaded {len(self.df)} samples from {input_file}")
+            len_df = len(self.df)
+            num_col = len(self.df.columns) - 1
+            self.logger.info(f"Loaded {len_df} samples from {input_file}")
+            self.logger.info(f"Each sample have {num_col} features ({len_df*num_col})")
         else:
             print(f"Loaded {len(self.df)} samples from {input_file}")
         return self
